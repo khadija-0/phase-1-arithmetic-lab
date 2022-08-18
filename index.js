7235417d134595e226746c6816e5f850604582e3
khadija.dahir <khadija.dahir@student.moringaschool.com>
2022-08-18 09:21:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random()*10) + 1;
+console.log(random)
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(4, 9, 14, 20);
